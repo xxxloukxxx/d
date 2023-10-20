25157d93284cf2fcf56c6e3d94528b2bc5d8b01d
Cedric <cedric.todaro@gmail.com>
2023-10-21 00:41:20 +0200

    sam. 21 oct. 2023 00:39:42 CEST

--- .dwm/powermenu.sh
+++ .dwm/powermenu.sh
@@ -2,13 +2,12 @@
 
 function powermenu {
     options="Shutdown\nReboot\nLock"
-    selected=$(echo -e $options | dmenu -i -fn "Hack:size=14")
+    selected=$(echo -e $options | dmenu -b -i -fn "Inconsolata:size=12")
 
     if [[ $selected = "Shutdown" ]]; then sudo poweroff
     elif [[ $selected = "Reboot" ]]; then sudo reboot
     elif [[ $selected = "Lock" ]]; then slock
     fi
-
 }
 
 powermenu

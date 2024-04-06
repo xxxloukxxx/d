193604f9ac47af4ba9b4459463eecb02a6c728e6
Cedric <cedric.todaro@gmail.com>
2024-04-06 19:01:51 +0200

    sam. 06 avril 2024 18:54:39 CEST

--- dev/dwm/config.def.h
+++ dev/dwm/config.def.h
@@ -39,7 +39,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact = 0.65; /* factor of master area size [0.05..0.95] */
+static const float mfact = 0.618; /* factor of master area size [0.05..0.95] */
 static const int nmaster = 1;		 /* number of clients in master area */
 static const int resizehints = 1;	 /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
@@ -70,10 +70,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL};
 static const char *termcmd[] = {"st", NULL};
 static const char *shutdowncmd[] = {"/home/cedric/.dwm/powermenu.sh", NULL};
-
 static const char *pavucmd[] = {"pavucontrol", NULL};
 static const char *flamecmd[] = {"flameshot", "gui", NULL};
-
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
@@ -90,52 +88,45 @@ Binding keys
 */
 static const Key keys[] = {
     /* modifier             key     function        argument */
-    {0, 121, spawn, {.v = mutevol}},								   // 121
-    {0, 122, spawn, {.v = downvol}},								   // 122
-    {0, 123, spawn, {.v = upvol}},									   // 123
-//	{MODKEY, 33, spawn, {.v = dmenucmd}},							   // p
-    {0, 191, spawn, {.v = dmenucmd}},							   // d
-    {0, 192, spawn, {.v = termcmd}},				               // Return
-    {MODKEY, 40, spawn, {.v = dmenucmd}},							   // d
-    {MODKEY, 36, spawn, {.v = termcmd}},				               // Return
-    {MODKEY | ControlMask | ShiftMask, 22, spawn, {.v = shutdowncmd}}, // BS
-    {MODKEY | ControlMask | ShiftMask, 33, spawn, {.v = pavucmd}},	   // p
-    {MODKEY | ControlMask | ShiftMask, 39, spawn, {.v = flamecmd}},	   // s
-    {MODKEY, 9, togglescratch, {.v = scratchpadcmd }},                 // Esc
-    {MODKEY, 56, togglebar, {0}},									   // b
-    {MODKEY, 44, focusstack, {.i = +1}},							   // j
-    {MODKEY, 45, focusstack, {.i = -1}},							   // k
-    {MODKEY, 113, focusstack, {.i = +1}},							   // j
-    {MODKEY, 114, focusstack, {.i = -1}},							   // k
-    {MODKEY, 55, incnmaster, {.i = +1}},							   // v
-//	{ MODKEY,               40,     incnmaster,     {.i = -1 } },      // d
-    {MODKEY, 43, setmfact, {.f = -0.05}},							   // h
-    {MODKEY, 46, setmfact, {.f = +0.05}},							   // l
-    {MODKEY | ShiftMask, 36, zoom, {0}},							   // Return
-    {MODKEY, 23, view, {0}},										   // Tab
-    {MODKEY | ShiftMask, 54, killclient, {0}},						   // c
-    {MODKEY | ShiftMask, 56, setlayout, {.v = &layouts[1]}},		   // b
-    {MODKEY | ShiftMask, 57, setlayout, {.v = &layouts[0]}},		   // n
-    {MODKEY | ShiftMask, 58, setlayout, {.v = &layouts[2]}},		   // m
-    {MODKEY, 65, setlayout, {0}},									   // space
+//    {0, 121, spawn, {.v = mutevol}},								   // 121 - Mute
+//    {0, 122, spawn, {.v = downvol}},								   // 122 - Voldn
+//    {0, 123, spawn, {.v = upvol}},									   // 123 - Volup
+//    {0, 191, spawn, {.v = dmenucmd}},							       // F13
+//    {0, 192, spawn, {.v = termcmd}},				                   // F14
+//    {MODKEY, 40, spawn, {.v = dmenucmd}},							   // Meta + d
+//    {MODKEY, 36, spawn, {.v = termcmd}},				               // Meta + Return
+    {MODKEY | ControlMask | ShiftMask, 22, spawn, {.v = shutdowncmd}}, // Meta + Ctrl + Shift + BS
+    {MODKEY | ControlMask | ShiftMask, 33, spawn, {.v = pavucmd}},	   // Meta + Ctrl + Shift + p
+    {MODKEY | ControlMask | ShiftMask, 39, spawn, {.v = flamecmd}},	   // Meta + Ctrl + Shift + s
+    {MODKEY, 9, togglescratch, {.v = scratchpadcmd }},                 // Meta + Esc
+    {MODKEY, 44, focusstack, {.i = +1}},							   // Meta + j
+    {MODKEY, 45, focusstack, {.i = -1}},							   // Meta + k
+    {MODKEY, 113, focusstack, {.i = +1}},							   // Meta + Left
+    {MODKEY, 114, focusstack, {.i = -1}},							   // Meta + Right
+    {MODKEY, 43, setmfact, {.f = -0.05}},							   // Meta + h
+    {MODKEY, 46, setmfact, {.f = +0.05}},							   // Meta + l
+    {MODKEY | ShiftMask, 36, zoom, {0}},							   // Meta + Shift + Return
+    {MODKEY, 23, view, {0}},										   // Meta + Tab
+    {MODKEY | ShiftMask, 54, killclient, {0}},						   // Meta + Shift + c
+    {MODKEY, 65, setlayout, {0}},									   // Meta + Shift + space
+    {MODKEY | ShiftMask, 57, setlayout, {.v = &layouts[0]}},		   // Meta + Shift + n
+    {MODKEY | ShiftMask, 56, setlayout, {.v = &layouts[1]}},		   // Meta + Shift + b
+    {MODKEY | ShiftMask, 58, setlayout, {.v = &layouts[2]}},		   // Meta + Shift + m
     {MODKEY | ShiftMask, 65, togglefloating, {0}},					   // space
-//	{ MODKEY,               19,     view,           {.ui = ~0 } },     // 0
-//	{ MODKEY|ShiftMask,     19,     tag,            {.ui = ~0 } },     // 0
-    {MODKEY, 59, focusmon, {.i = -1}},								   // comma
-    {MODKEY, 60, focusmon, {.i = +1}},								   // period
-    {MODKEY | ShiftMask, 59, tagmon, {.i = -1}},					   // comma
-    {MODKEY | ShiftMask, 60, tagmon, {.i = +1}},					   // period
-    TAGKEYS(24, 0)													   // q
-    TAGKEYS(25, 1)													   // w
-    TAGKEYS(26, 2)													   // e
-    TAGKEYS(27, 3)													   // r
-    TAGKEYS(28, 4)													   // t
-    TAGKEYS(29, 5)													   // y
-    TAGKEYS(30, 6)													   // u
-    TAGKEYS(31, 7)													   // i
-    TAGKEYS(32, 8)													   // o
-    TAGKEYS(33, 9)													   // p
-    //	{ MODKEY|ShiftMask,     24,     quit,           {0} },          // q
+    {MODKEY, 59, focusmon, {.i = -1}},								   // Meta + comma
+    {MODKEY, 60, focusmon, {.i = +1}},								   // Meta + period
+    {MODKEY | ShiftMask, 59, tagmon, {.i = -1}},					   // Meta + Shift + comma
+    {MODKEY | ShiftMask, 60, tagmon, {.i = +1}},					   // Meta + Shift + period
+    TAGKEYS(24, 0)													   // Meta (+ Shift) + q
+    TAGKEYS(25, 1)													   // Meta (+ Shift) + w
+    TAGKEYS(26, 2)													   // Meta (+ Shift) + e
+    TAGKEYS(27, 3)													   // Meta (+ Shift) + r
+    TAGKEYS(28, 4)													   // Meta (+ Shift) + t
+    TAGKEYS(29, 5)													   // Meta (+ Shift) + y
+    TAGKEYS(30, 6)													   // Meta (+ Shift) + u
+    TAGKEYS(31, 7)													   // Meta (+ Shift) + i
+    TAGKEYS(32, 8)													   // Meta (+ Shift) + o
+    TAGKEYS(33, 9)													   // Meta (+ Shift) + p
 };
 
 /* button definitions */

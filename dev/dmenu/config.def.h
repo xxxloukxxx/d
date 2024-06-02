d55211f986444f262af17b35a8de5b5d88ec00cc
Cedric <cedric.todaro@gmail.com>
2024-06-02 12:50:02 +0200

    dim. 02 juin 2024 12:45:27 CEST

--- dev/dmenu/config.def.h
+++ dev/dmenu/config.def.h
@@ -10,13 +10,22 @@ static const char *fonts[] = { "monospace:pixelsize=20"};
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeNorm] = { "#bbbbbb", "#000000" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeSelHighlight] = { "#ffc978", "#005577" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
 };
+//static const char *colors[SchemeLast][2] = {
+//	/*     fg         bg       */
+//	[SchemeNorm] = { "#bbbbbb", "#222222" },
+//	[SchemeSel] = { "#eeeeee", "#005577" },
+//	[SchemeSelHighlight] = { "#ffc978", "#005577" },
+//	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+//	[SchemeOut] = { "#000000", "#00ffff" },
+//	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
+//};
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
